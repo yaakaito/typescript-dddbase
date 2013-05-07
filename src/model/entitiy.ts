6015a027569bc1d662a8b71e9931ebfaaf2ade5b
KAZUMA Ukyo <yaakaito@gmail.com>
2013-05-07 20:48:54 +0900

    Add equals

--- src/model/entitiy.ts
+++ src/model/entitiy.ts
@@ -3,13 +3,28 @@
 module DDD {
 
     export interface Entity {
+        getIdentify(): Identify;
+        equals(that: Entity): bool;
     }
 
     export class AbstractEntity implements Entity {
         
-        constructor(public indetify: Identify) {
+        constructor(private identify: Identify) { }
 
+        public getIdentify(): Identify {
+           return this.identify;
         }
+
+        public equals(that: Entity): bool {
+            if (that == null) {
+                return false;
+            }
+            if (this == that) {
+                return true;
+            }
+            return this.identify.equals(that.getIdentify());
+        }
+
     }
 
 }
--- src/model/identify.ts
+++ src/model/identify.ts
@@ -1,6 +1,27 @@
 module DDD {
 
     export interface Identify {
-        value() : any; // TODO:
+        getValue(): any;
+        equals(that: Identify): bool;
+    }
+
+    export class AbstractIdentify implements Identify {
+
+        constructor(private value: any) { }
+
+        public getValue(): any {
+            return this.value;
+        }
+
+        public equals(that: any) {
+            if (that == null) {
+                return false;
+            }
+            if (this == that) {
+                return true;
+            }
+
+            return this.value === that.getValue();
+        }
     }
 }
