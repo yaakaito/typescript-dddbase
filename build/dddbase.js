5a8877e60a07e94ab1c0bd2f91b972861cbf877e
KAZUMA Ukyo <yaakaito@gmail.com>
2013-05-10 21:07:38 +0900

    Add AsyncRepository store/resolve test.

--- build/dddbase.js
+++ build/dddbase.js
@@ -1,26 +1,4 @@
-var DDD;
-(function (DDD) {
-    var AbstractEntity = (function () {
-        function AbstractEntity(identify) {
-            this.identify = identify;
-        }
-        AbstractEntity.prototype.getIdentify = function () {
-            return this.identify;
-        };
-        AbstractEntity.prototype.equals = function (that) {
-            if(that == null) {
-                return false;
-            }
-            if(this == that) {
-                return true;
-            }
-            return this.identify.equals(that.getIdentify());
-        };
-        return AbstractEntity;
-    })();
-    DDD.AbstractEntity = AbstractEntity;    
-})(DDD || (DDD = {}));
-;var __extends = this.__extends || function (d, b) {
+var __extends = this.__extends || function (d, b) {
     function __() { this.constructor = d; }
     __.prototype = b.prototype;
     d.prototype = new __();
@@ -55,4 +33,78 @@ var DDD;
         return NumberIdentify;
     })(AbstractIdentify);
     DDD.NumberIdentify = NumberIdentify;    
+})(DDD || (DDD = {}));
+var DDD;
+(function (DDD) {
+    var AbstractEntity = (function () {
+        function AbstractEntity(identify) {
+            this.identify = identify;
+        }
+        AbstractEntity.prototype.getIdentify = function () {
+            return this.identify;
+        };
+        AbstractEntity.prototype.equals = function (that) {
+            if(that == null) {
+                return false;
+            }
+            if(this == that) {
+                return true;
+            }
+            return this.identify.equals(that.getIdentify());
+        };
+        return AbstractEntity;
+    })();
+    DDD.AbstractEntity = AbstractEntity;    
+})(DDD || (DDD = {}));
+var DDD;
+(function (DDD) {
+    var OnMemoryRepository = (function () {
+        function OnMemoryRepository() {
+            this.entities = {
+            };
+        }
+        OnMemoryRepository.prototype.resolveWithIdentify = function (identify) {
+            return this.entities[identify.getValue()];
+        };
+        OnMemoryRepository.prototype.store = function (entity) {
+            this.entities[entity.getIdentify().getValue()] = entity;
+            return entity;
+        };
+        OnMemoryRepository.prototype.deleteByEntity = function (entity) {
+            this.deleteByIdentify(entity.getIdentify());
+        };
+        OnMemoryRepository.prototype.deleteByIdentify = function (identify) {
+            delete this.entities[identify.getValue()];
+        };
+        return OnMemoryRepository;
+    })();
+    DDD.OnMemoryRepository = OnMemoryRepository;    
+})(DDD || (DDD = {}));
+var DDD;
+(function (DDD) {
+    var AsyncOnMemoryRepository = (function (_super) {
+        __extends(AsyncOnMemoryRepository, _super);
+        function AsyncOnMemoryRepository(createResolver) {
+                _super.call(this);
+            this.createResolver = createResolver;
+        }
+        AsyncOnMemoryRepository.prototype.storeAsync = function (entity) {
+            this.store(entity);
+            return this.createResolver().resolve(entity);
+        };
+        AsyncOnMemoryRepository.prototype.resolveAsyncWithIdentify = function (identify) {
+            var entity = this.resolveWithIdentify(identify);
+            return this.createResolver().resolve(entity);
+        };
+        AsyncOnMemoryRepository.prototype.deleteAsyncByEntity = function (entity) {
+            this.deleteByEntity(entity);
+            return this.createResolver().resolve();
+        };
+        AsyncOnMemoryRepository.prototype.deleteAsyncByIdentify = function (identify) {
+            this.deleteByIdentify(identify);
+            return this.createResolver().resolve();
+        };
+        return AsyncOnMemoryRepository;
+    })(DDD.OnMemoryRepository);
+    DDD.AsyncOnMemoryRepository = AsyncOnMemoryRepository;    
 })(DDD || (DDD = {}));
---
+++ test/async_repository.ts
@@ -0,0 +1,88 @@
+/// <reference path="../src/identify.ts" />
+/// <reference path="../src/entity.ts" />
+/// <reference path="../src/async_repository.ts" />
+/// <reference path="../d.ts/mocha.d.ts" />
+/// <reference path="../d.ts/chai.d.ts" />
+
+module DDD {
+
+    class Person extends AbstractEntity {
+
+        constructor(identify: DDD.Identify, public name: string) {
+            super(identify);
+        }
+    }
+
+    class SimpleResolver implements Resolver {
+
+        private target: any = null;
+        private resolved: bool = false;
+        private callback: (target?: any) => any = null;
+
+        public resolve(entity: Entity): Resolver;
+        public resolve(identify: Identify): Resolver;
+        public resolve(): Resolver;
+        public resolve(target?: any): Resolver {
+            this.target = target;
+            this.fireCallback();
+            this.resolved = true;
+            return this;
+        }
+
+        public onComplete(callback: (target?: any) => any): Resolver {
+            this.callback = callback;
+            if (this.resolved) {
+                this.fireCallback();
+            }
+            return this;
+        }
+
+        private fireCallback() {
+            if (this.callback) {
+                if (this.target) {
+                    this.callback(this.target);
+                }
+                else {
+                    this.callback();
+                }
+            }
+        }
+    }
+
+    describe('AsyncRepository', () => {
+
+        var expect = chai.expect;
+
+        describe('OnMemoryRepository', () => {
+
+            var identify;
+            var name;
+            var person;
+            beforeEach(() => {
+                identify = new NumberIdentify(10);
+                name = 'yaakaito';
+                person = new Person(identify, name);
+            });
+
+            describe('With SimpleResolver', () => {
+
+                var repository;
+                beforeEach(() => {
+                    repository = new AsyncOnMemoryRepository(() => new SimpleResolver());
+
+                });
+
+                it('can async store entity and async resolve it', (done) => {
+                    repository.storeAsync(person).onComplete((entity: Entity) => {
+                        expect(entity).to.equal(person);
+                        repository.resolveAsyncWithIdentify(identify).onComplete((entity: Entity) => {
+                            expect(entity).to.equal(person);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+}
